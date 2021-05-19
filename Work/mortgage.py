ba87da12b2d9254d9cb5df0ceb7a3f8cd4779152
Vivek Patel <vivek.d.ptl@gmail.com>
2021-05-19 13:18:20 -0600

    Exercise 1.8 completed

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,13 +1,22 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.8
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+month = 0
 
 while principal > 0:
+    month += 1
+    if month <= 12:
+        payment = 3684.11
+    else:
+        payment = 2684.11    
+ 
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
+    
+    
 
-print('Total paid', total_paid)
+print('Total paid', total_paid, 'for', month)
