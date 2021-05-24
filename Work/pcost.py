c8364702b2179367f0a830e2fa8ac2ab0163e060
Vivek Patel <vivek.d.ptl@gmail.com>
2021-05-24 12:28:33 -0600

    Exercise 1.33 completed

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,25 @@
 # pcost.py
 #
-# Exercise 1.32
+# Exercise 1.33
+import sys
 import csv
-f = open('Data/portfolio.csv')
-rows = csv.reader(f)
-headers = next(rows)
-total_sum = 0
-for row in rows:
-    total_sum = total_sum + float(row[1]) * float(row[2])
-print(f'Total cost {total_sum}')     
 
+def portfolio_cost(filename):
+    f = open(filename)
+    rows = csv.reader(f)
+    headers = next(rows)
+    total_sum = 0
+    for row in rows:
+        try:
+             total_sum = total_sum + float(row[1]) * float(row[2])
+        except ValueError:
+            print("Couldn't process the balnk string data")
+    return total_sum
 
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
