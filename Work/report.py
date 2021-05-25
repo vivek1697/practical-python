cf34cab018c9a9ba8139f7b8c54e95f0e9528696
Vivek Patel <vivek.d.ptl@gmail.com>
2021-05-25 13:54:19 -0600

    Exercise 2.6 completed with the implement of dic for pricing.csv

--- Work/report.py
+++ Work/report.py
@@ -1,24 +1,39 @@
 # report.py
 #
-# Exercise 2.5
+# Exercise 2.6
 import csv
 
-def read_portfolio(filename):
+# def read_portfolio(filename):
     
-    portfolio = []
+#     portfolio = []
+#     #Open the csv file and read the data from it 
+#     with open(filename, 'rt') as f:
+#         rows = csv.reader(f)
+#         headers = next(rows)
+#         #Run a loop on each row convert it into the dic and append that into the portfolio list
+#         for row in rows:
+#             holding = {'name' : row[0], 'shares' : int(row[1]), 'price'  : float(row[2])}
+#             portfolio.append(holding)
+#         #Calculate the total valuation of Portfolio    
+#         total = 0.0
+#         for s in portfolio:
+#            total += s['shares'] * s['price']
+#     return print(total)
+
+# read_portfolio('Data/portfolio.csv')
+
+def read_prices(filename):
+    prices = {}
     #Open the csv file and read the data from it 
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
-        #Run a loop on each row convert it into the dic and append that into the portfolio list
+        #Run a loop on each row append into the dic
         for row in rows:
-            holding = {'name' : row[0], 'shares' : int(row[1]), 'price'  : float(row[2])}
-            portfolio.append(holding)
-        #Calculate the total valuation of Portfolio    
-        total = 0.0
-        for s in portfolio:
-           total += s['shares'] * s['price']
-    return print(total)
+            if row != []:
+                k, v = row
+                prices[k] = v
+       
+    return print(prices)
 
-read_portfolio('Data/portfolio.csv')
-
+read_prices('Data/prices.csv')
