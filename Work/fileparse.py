21031d4aecade5808c0cb569f47acc3375d4524f
Vivek Patel <vivek.d.ptl@gmail.com>
2021-05-31 09:39:35 -0600

    Exercise 3.7 done

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,15 +1,15 @@
 # fileparse.py
 #
-# Exercise 3.6
+# Exercise 3.7
 
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=False):
+def parse_csv(filename, select=None, types=None, has_headers=False, delimiter = ','):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
 
         # Read the file headers
         headers = next(rows) if has_headers else []
@@ -19,8 +19,7 @@ def parse_csv(filename, select=None, types=None, has_headers=False):
         if select:
             indices = [headers.index(colname) for colname in select]
             headers = select
-        else:
-            indices = []
+        
 
         records = []
         for row in rows:
@@ -45,6 +44,6 @@ def parse_csv(filename, select=None, types=None, has_headers=False):
         print(records)
     return records
 
-portfolio = parse_csv('Data/prices.csv', select=None, types=[str,float], has_headers=False)
+portfolio = parse_csv('Data/portfolio.dat', select=None, types=[str,float], has_headers=True, delimiter = ' ')
 
 
